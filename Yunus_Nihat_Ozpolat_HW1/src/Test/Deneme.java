0a6d49b18ac4d5957a839cab4b8c9ca3b0292790
Yunus Nihat Özpolat <yunusnihat6775@hotmail.com>
2023-01-25 16:03:07 +0300

    OOP_Java

---
+++ Yunus_Nihat_Ozpolat_HW1/src/Test/Deneme.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Test;
+import java.util.Scanner;
+/**
+ *
+ * @author PC
+ */
+public class Deneme {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Kuba kuba1 = new Kuba();
+        Kuba kuba2 = new Kuba();
+        Galli galli1 = new Galli();
+        Galli galli2 = new Galli();
+        //kuba1.GetCurrency();
+        //galli1.GetCurrency();
+        //galli2.GetCurrency();
+        //galli1.Add(galli2);
+        kuba1.GetCurrency();
+        kuba2.GetCurrency();
+        kuba1.Add(kuba2);
+        //kuba2.GetCurrency();
+        //kuba2.GetCurrency();
+        //galli1.Subtract(galli2);
+        //galli1.ShowCurrency();
+        //kuba2.GetCurrency();
+        //kuba2.Subtract(kuba1);
+        //kuba2.ShowCurrency();
+        //kuba1.Convert().ShowCurrency();
+        //kuba1=galli1.Convert();
+        //kuba1.ShowCurrency();
+        //kuba1.Subtract(kuba2);
+        //kuba1.Add(galli1);
+        //kuba2.GetCurrency();
+        //kuba1.Add(kuba2);
+        //kuba1.ShowCurrency();
+        //kuba1.Subtract(kuba2);
+        //galli1 = galli1.Convert();
+        //galli1.Add(kuba1);
+        
+        //kuba1.ShowCurrency();
+        
+
+
+
+
+
+        
+
+
+    }
+
+    
+}
---
+++ Yunus_Nihat_Ozpolat_HW1/src/Test/Galli.java
@@ -0,0 +1,288 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test;
+import java.util.Scanner;
+/**
+ *
+ * @author PC
+ */
+public class Galli {
+    private int Currency;
+    public void Add(Galli newGalli) {
+        String s = Integer.toString(newGalli.Currency);
+        String[] sArray = s.split("");
+        int[] sArray2 = new int[s.length()];
+        int number = 0;
+        int index  = 0;
+        for(int i = s.length() -1 ; i >=0 ; i-- ){
+            number = Integer.parseInt(sArray[i]) * (int)Math.pow(9,index);
+            sArray2[index] = number;
+            index++;
+        }
+        String s1 = Integer.toString(this.Currency);
+        String[] sArray1 = s1.split("");
+        int[] sArray3 = new int[s1.length()];
+        int number1 = 0;
+        int index1  = 0;
+        for(int i = s1.length() -1  ; i >= 0; i-- ){
+            number1 = Integer.parseInt(sArray1[i]) * (int)Math.pow(9,index1);
+            sArray3[index1] = number1;
+            index1++;
+        }
+        int sum1  = 0;
+        for(int i = 0; i < sArray2.length; i++){
+
+            sum1 += sArray2[i];
+        }
+        int sum2  = 0;
+        for(int i = 0; i < sArray3.length; i++){
+            sum2 += sArray3[i];
+        }
+        int allSum = sum1 + sum2;
+        String stringCurrency = Integer.toString(allSum);
+        int division = allSum / 9;
+        int mod = allSum % 9;
+        int[] arrayKubaCurrency = new int[9];
+        arrayKubaCurrency[0] = mod;
+        int arrayKubaCurrencyIndex = 1;
+        while (arrayKubaCurrencyIndex < arrayKubaCurrency.length) {
+            if (division < 9) {
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = division;
+                break;
+            } else {
+                mod = division % 9;
+                division = division / 9;
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = mod;
+            }
+            arrayKubaCurrencyIndex++;
+        }
+        int[] intArray = new int[9];
+        for (int i = arrayKubaCurrency.length - 1; i >= 0; i--) {
+            intArray[i] = arrayKubaCurrency[i];
+        }
+        String newCurrency = "";
+        for(int i = intArray.length -1 ; i >= 0 ; i--){
+            newCurrency += intArray[i];
+        }
+        int newCurrency2 = Integer.parseInt(newCurrency);
+        this.Currency = newCurrency2;
+        ShowCurrency();
+    }
+    public void Add(Kuba newKuba) {
+        int temp = Currency;
+        String s = Integer.toString(newKuba.TakeCurrency(temp));
+        String[] sArray = s.split("");
+        int[] sArray2 = new int[s.length()];
+        int number = 0;
+        int index  = 0;
+        for(int i = s.length() - 1 ; i >= 0 ; i-- ){
+            number = Integer.parseInt(sArray[i]) * (int)Math.pow(7,index);
+            sArray2[i] = number;
+            index++;
+        }
+        System.out.println(sArray2[1]);
+        String s1 = Integer.toString(this.Currency);
+        String[] sArray1 = s1.split("");
+        int[] sArray3 = new int[s1.length()];
+        int number1 = 0;
+        int index1  = 0;
+        for(int i = s1.length() - 1 ; i >= 0 ; i-- ){
+            number1 = Integer.parseInt(sArray1[i]) * (int)Math.pow(9,index1);
+            sArray3[i] = number1;
+            index1++;
+        }
+        int sum1  = 0;
+        for(int i = 0; i < sArray2.length; i++){
+            sum1 += sArray2[i];
+        }
+        System.out.println(sum1);
+        int newSum1 =  sum1 / 2;
+        int sum2  = 0;
+        for(int i = 0; i < sArray3.length; i++){
+            sum2 += sArray3[i];
+        }
+        System.out.println(sum2);
+        //int newSum1 =  sum2/2;
+        int allSum = newSum1 + sum2;
+        String stringCurrency = Integer.toString(allSum);
+        int division = allSum / 9;
+        int mod = allSum % 9;
+        int[] arrayKubaCurrency = new int[9];
+        arrayKubaCurrency[0] = mod;
+        int arrayKubaCurrencyIndex = 1;
+        while (arrayKubaCurrencyIndex < arrayKubaCurrency.length) {
+            if (division < 9) {
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = division;
+                break;
+            } else {
+                mod = division % 9;
+                division = division / 9;
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = mod;
+            }
+            arrayKubaCurrencyIndex++;
+        }
+        int[] intArray = new int[9];
+        for (int i = arrayKubaCurrency.length - 1; i >= 0; i--) {
+            intArray[i] = arrayKubaCurrency[i];
+        }
+        String newCurrency = "";
+        for(int i = intArray.length -1 ; i >= 0 ; i--){
+            newCurrency += intArray[i];
+        }
+        int newCurrency2 = Integer.parseInt(newCurrency);
+        this.Currency = newCurrency2;
+        ShowCurrency();
+    }
+
+    public void GetCurrency() {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            boolean isFeasibleKuba = true;
+            System.out.print("Enter a galli currency: ");
+            int kubaInput = input.nextInt();
+            String kubaCurrency = Integer.toString(kubaInput);
+            String[] currencyArray = kubaCurrency.split("");
+            for (int i = 0; i < kubaCurrency.length(); i++) {
+                if (kubaCurrency.length() > 9 || currencyArray[i].equals("9")) {
+                    System.out.println("Error: Icorrect galli currency.Please enter new galli currency");
+                    isFeasibleKuba = false;
+                    break;
+                }
+            }
+            if (isFeasibleKuba) {
+                this.Currency = kubaInput;
+                break;
+            }
+        }
+    }
+    public void GetCurrency(Integer newGalli) {
+        String newKubacurrency = Integer.toString(newGalli);
+        String[] newKubaCurrency = newKubacurrency.split("");
+        for (int i = 0; i < newKubacurrency.length(); i++) {
+            if (newKubacurrency.length() > 9 || newKubaCurrency[i].equals("9")) {
+                System.out.println("Error: Icorrect galli currency. Please enter new galli currency");
+                break;
+            }
+        }
+        this.Currency = newGalli;
+    }
+    public void ShowCurrency() {
+        System.out.println(this.Currency);
+    }
+    public void Subtract(Galli newGalli) {
+        String s = Integer.toString(newGalli.Currency);
+        String[] sArray = s.split("");
+        int[] sArray2 = new int[s.length()];
+        int number = 0;
+        int index  = 0;
+        for(int i = s.length() - 1  ; i >= 0 ; i-- ){
+            number = Integer.parseInt(sArray[i]) * (int)Math.pow(9,index);
+            sArray2[index] = number;
+            index++;
+        }
+        String s1 = Integer.toString(this.Currency);
+        String[] sArray1 = s1.split("");
+        int[] sArray3 = new int[s1.length()];
+        int number1 = 0;
+        int index1  = 0;
+        for(int i = s1.length() - 1 ; i >= 0 ; i-- ){
+            number1 = Integer.parseInt(sArray1[i]) * (int)Math.pow(9,index1);
+            sArray3[index1] = number1;
+            index1++;
+        }
+        int sum1  = 0;
+        for(int i = 0; i < sArray2.length; i++){
+            sum1 += sArray2[i];
+        }
+        int sum2  = 0;
+        for(int i = 0; i < sArray3.length; i++){
+            sum2 += sArray3[i];
+        }
+        int allSum = 0;
+        if(sum1 - sum2 < 0){
+            allSum = sum2 - sum1;
+        } else if (sum2 - sum1 < 0) {
+            allSum = sum1 - sum2;
+        }
+        String stringCurrency = Integer.toString(allSum);
+        int division = allSum / 9;
+        int mod = allSum % 9;
+        int[] arrayKubaCurrency = new int[9];
+        arrayKubaCurrency[0] = mod;
+        int arrayKubaCurrencyIndex = 1;
+        while (arrayKubaCurrencyIndex < arrayKubaCurrency.length) {
+            if (division < 9) {
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = division;
+                break;
+            } else {
+                mod = division % 9;
+                division = division / 9;
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = mod;
+            }
+            arrayKubaCurrencyIndex++;
+        }
+        int[] intArray = new int[9];
+        for (int i = arrayKubaCurrency.length - 1; i >= 0; i--) {
+            intArray[i] = arrayKubaCurrency[i];
+        }
+        String newCurrency = "";
+        for(int i = intArray.length - 1; i >= 0 ; i--){
+            newCurrency += intArray[i];
+        }
+        int newCurrency2 = Integer.parseInt(newCurrency);
+        this.Currency = newCurrency2;
+        ShowCurrency();
+    }
+    public Kuba Convert() {
+        Kuba newGalli = new Kuba();
+        String s1 = Integer.toString(this.Currency);
+        String[] sArray1 = s1.split("");
+        int[] sArray3 = new int[s1.length()];
+        int number1 = 0;
+        int index1  = 0;
+        for(int i = s1.length() - 1 ; i >= 0 ; i-- ){
+            number1 = Integer.parseInt(sArray1[i]) * (int)Math.pow(9,index1);
+            sArray3[index1] = number1;
+            index1++;
+        }
+        int sum1  = 0;
+        for(int i = 0; i < sArray3.length; i++){
+            sum1 += sArray3[i];
+        }
+        int kuba = sum1 * 2;
+        String stringCurrency = Integer.toString(kuba);
+        int division = kuba / 7;
+        int mod = kuba % 7;
+        int[] arrayKubaCurrency = new int[7];
+        arrayKubaCurrency[0] = mod;
+        int arrayKubaCurrencyIndex = 1;
+        while (arrayKubaCurrencyIndex < arrayKubaCurrency.length) {
+            if (division < 7) {
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = division;
+                break;
+            } else {
+                mod = division % 7;
+                division = division / 7;
+                arrayKubaCurrency[arrayKubaCurrencyIndex] = mod;
+            }
+            arrayKubaCurrencyIndex++;
+        }
+        int[] intArray = new int[7];
+        for (int i = arrayKubaCurrency.length - 1; i >= 0; i--) {
+            intArray[i] = arrayKubaCurrency[i];
+        }
+        String newCurrency = "";
+        for(int i = intArray.length - 1; i >= 0 ; i--){
+            newCurrency += intArray[i];
+        }
+        int newCurrency2 = Integer.parseInt(newCurrency);
+        newGalli.TakeCurrency(newCurrency2) ;
+        return newGalli;
+    }
+        public int TakeCurrency(int Money){
+        Currency = Money;
+        return Currency;
+    }
+}
